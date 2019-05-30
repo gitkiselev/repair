4da285af518a3c4766cb825b8fb20a5e65d9d647
gitkiselev <482003@mail.ru>
2019-05-30 23:18:31 +0300

    Исправил JS

--- src/js/main.js
+++ src/js/main.js
@@ -1,18 +1,22 @@
 var button = document.querySelector('#button');
 var modal = document.querySelector('#modal');
 var close = document.querySelector('#close');
-
+var idSet;
 button.addEventListener('click', function() {
-    modal.classList.add('modal_active');
-    /** */
-    var idSet = setTimeout(function() {
-        modal.classList.remove('modal_active');
-    }, 5000);
-    clearTimeout('idSet');
+    openModal();
 });
 
 close.addEventListener('click', function() {
+    closeModal();
+});
+function openModal() {
+    modal.classList.add('modal_active');
+    idSet = setTimeout(function() {
+        modal.classList.remove('modal_active');
+    }, 5000);
+}
+function closeModal() {
     modal.classList.remove('modal_active');
     /*Очистим интервал, если закрыли раньше истечения таймера*/
-    clearTimeout('idSet');
-});
+    clearTimeout(idSet);
+}
